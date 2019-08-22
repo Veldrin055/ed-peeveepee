39d377424bb82aa88ef7ce8fc860966d615970c3
Daniel Morrison <daniel.morrison@oramon.nl>
2019-08-22 20:47:55 +1000

    App files

---
+++ src/renderer/components/Application/index.tsx
@@ -0,0 +1,67 @@
+import { hot } from 'react-hot-loader/root'
+import * as React from 'react'
+import { ThemeProvider } from '@material-ui/styles'
+import NavBar from '../NavBar'
+import theme from '../theme'
+import Cmdr from '../../containers/CmdrContainer'
+import IFF from '../../containers/IffContainer'
+import Loading from './Loading'
+import { Typography } from '@material-ui/core'
+
+interface PageProps {
+  children?: React.ReactNode
+}
+
+interface ApplicationProps {
+  ready: boolean
+}
+
+const Page = (props: PageProps) => (
+  <Typography component="div" style={{ padding: 8 * 3 }}>
+    {props.children}
+  </Typography>
+)
+
+const Application = ({ ready = false }: ApplicationProps) => {
+  const [value, setValue] = React.useState(0)
+
+  function handleChange(event: React.ChangeEvent<{}>, newValue: number) {
+    setValue(newValue)
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div style={{ maxHeight: '100vh' }}>
+        {ready ? (
+          <div>
+            <NavBar value={value} handleChange={handleChange} />
+            {value === 0 && (
+              <Page>
+                <Cmdr />
+              </Page>
+            )}
+            {value === 1 && (
+              <Page>
+                <IFF />
+              </Page>
+            )}
+            {value === 2 && (
+              <Page>
+                <p>Beacon</p>
+              </Page>
+            )}
+            {value === 3 && (
+              <Page>
+                <p>Games</p>
+              </Page>
+            )}
+          </div>
+        ) : (
+          <Loading />
+        )}
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default hot(Application)
