4306e51243d8677d0ade4f131941432ae7028112
Daniel Morrison <daniel.morrison@oramon.nl>
2019-09-11 16:25:39 +1000

    Set secondary to orange

--- src/renderer/components/theme.ts
+++ src/renderer/components/theme.ts
@@ -1,10 +1,11 @@
 import { createMuiTheme } from '@material-ui/core'
-import { purple } from '@material-ui/core/colors'
+import { purple, orange } from '@material-ui/core/colors'
 import createTypography from '@material-ui/core/styles/createTypography'
 import createPalette from '@material-ui/core/styles/createPalette'
 
 const palette = createPalette({
   primary: purple,
+  secondary: orange,
   type: 'dark',
   background: {
     default: '#616161',
