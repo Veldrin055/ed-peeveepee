d11b698e30032c1d6ecb651b99113fa0a634ba34
Daniel Morrison <daniel.morrison@siteminder.com>
2019-10-10 21:47:17 +1100

    Resilient ws server connection

--- src/renderer/server/index.ts
+++ src/renderer/server/index.ts
@@ -1,39 +1,59 @@
 class Server {
-  ws: WebSocket
+  private ws!: WebSocket
   subscriptions: Map<string, (data: any) => void>
+  private buffer: any[]
+  private ready: boolean
 
   constructor() {
-    // this.ws = new WebSocket('ws://ed-pvp-server.herokuapp.com')
-    this.ws = new WebSocket('ws://localhost:3000')
+    this.ready = false
+    this.buffer = []
     this.subscriptions = new Map()
+    this.connect()
+  }
+
+  // Send the data if connection is open, otherwise add it to the buffer
+  send(data: any) {
+    if (this.ready) {
+      this.ws.send(JSON.stringify(data))
+    } else {
+      this.buffer.push(data)
+    }
+  }
+
+  // Subscribe to messages for a given type
+  onmessage(type: string, callback: (data: any) => void) {
+    this.subscriptions.set(type, callback)
+  }
+
+  removeListener(type: string) {
+    this.subscriptions.delete(type)
+  }
+
+  connect() {
+    this.ready = false
+    this.ws = new WebSocket('ws://ed-pvp-server.herokuapp.com')
+
+    this.ws.onopen = () => {
+      // flush the buffer and set ready
+      for (let i = 0; i < this.buffer.length; i += 1) {
+        this.ws.send(JSON.stringify(this.buffer.shift()))
+      }
+      this.ready = true
+    }
+
+    this.ws.onclose = e => {
+      console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason)
+      setTimeout(() => this.connect(), 1000)
+    }
+
     this.ws.onmessage = ({ data }) => {
-      console.log('message received', data)
       const { type, msg } = JSON.parse(data)
+
       const sub = this.subscriptions.get(type)
       if (sub) {
         sub(msg)
       }
     }
-  }
-
-  send(data: any) {
-    console.log('sending', data)
-    return new Promise(async (resolve, reject) => {
-      let attempts = 3
-      while (this.ws.readyState !== this.ws.OPEN) {
-        attempts = attempts - 1
-        if (attempts === 0) {
-          reject('Server connection timed out')
-        }
-        await setTimeout(50)
-      }
-      this.ws.send(JSON.stringify(data))
-      resolve()
-    })
-  }
-
-  onmessage(type: string, callback: (data: any) => void) {
-    this.subscriptions.set(type, callback)
   }
 }
 
